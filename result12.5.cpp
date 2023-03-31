b639762352d749623439a8f15f05af924dca3bca
Dev-Mood <126387763+Dev-Mood@users.noreply.github.com>
2023-03-31 12:23:15 +0800

    Add files via upload

---
+++ result12.5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    string message = "\nLearn from your mistakes!";
+    cout << message << endl;
+
+    int len = message.length();
+    cout << "Length of the string: " << len << endl;
+
+    cout << "And a random number in addition: " << endl;
+    float b = 12.5;
+    cout << "Random number: " << b << endl;
+
+    return 0;
+}
